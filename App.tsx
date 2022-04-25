5f54ca82e87ef5d5f413f448f29609f08ff04747
swetha-sankar <swetha@udel.edu>
2022-04-25 19:35:36 -0400

    base camera template

--- App.tsx
+++ App.tsx
@@ -1,13 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar'
+import React from 'react'
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native'
+//const [startCamera,setStartCamera] = React.useState(false)
+import {Camera} from 'expo-camera'
 
+
+const __startCamera = ()=>{
+  let camera: Camera
+  <Camera
+    style={{flex: 1,width:"100%"}}
+    ref={(r) => {
+    camera = r
+    }}
+    ></Camera>
+}
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#fff',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <TouchableOpacity
+        onPress={__startCamera}
+          style={{
+            width: 130,
+            borderRadius: 4,
+            backgroundColor: '#14274e',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 40
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              fontWeight: 'bold',
+              textAlign: 'center'
+            }}
+          >
+            Take picture
+          </Text>
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +59,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
