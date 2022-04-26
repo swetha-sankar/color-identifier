2d4a4ea30d2bcde2bf4219b33845937b0cef690e
swetha-sankar <swetha@udel.edu>
2022-04-25 20:23:32 -0400

    updates to the camera

--- App.tsx
+++ App.tsx
@@ -1,54 +1,104 @@
-import {StatusBar} from 'expo-status-bar'
+import { StatusBar } from 'expo-status-bar'
+import { registerRootComponent } from 'expo';
 import React from 'react'
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native'
-//const [startCamera,setStartCamera] = React.useState(false)
-import {Camera} from 'expo-camera'
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native'
+import { Camera } from 'expo-camera'
 
-
-const __startCamera = ()=>{
-  let camera: Camera
-  <Camera
-    style={{flex: 1,width:"100%"}}
-    ref={(r) => {
-    camera = r
-    }}
-    ></Camera>
-}
 export default function App() {
+  const [startCamera, setStartCamera] = React.useState(false)
+  const [previewVisible, setPreviewVisible] = React.useState(false)
+  const [capturedImage, setCapturedImage] = React.useState<any>(null)
+  let camera: Camera
+
+  const __takePicture = async () => {
+    if (!camera) return
+    const photo = await camera.takePictureAsync()
+    console.log(photo)
+    setPreviewVisible(true)
+    setCapturedImage(photo)
+  }
+
+  const __startCamera = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync()
+    if (status === 'granted') {
+      setStartCamera(true)
+    } else {
+      Alert.alert('Access denied')
+    }
+  }
   return (
     <View style={styles.container}>
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#fff',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <TouchableOpacity
-        onPress={__startCamera}
+      {startCamera ? (
+        <Camera
+          style={{ flex: 1, width: "100%" }}
+          ref={(r) => {
+            camera = r
+          }}
+        ></Camera>
+      ) : (
+        <View
           style={{
-            width: 130,
-            borderRadius: 4,
-            backgroundColor: '#14274e',
-            flexDirection: 'row',
+            flex: 1,
+            backgroundColor: '#fff',
             justifyContent: 'center',
-            alignItems: 'center',
-            height: 40
+            alignItems: 'center'
           }}
         >
-          <Text
+          <TouchableOpacity
+            onPress={__startCamera}
             style={{
-              color: '#fff',
-              fontWeight: 'bold',
-              textAlign: 'center'
+              width: 130,
+              borderRadius: 4,
+              backgroundColor: '#14274e',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 40
             }}
           >
-            Take picture
-          </Text>
-        </TouchableOpacity>
-      </View>
+            <Text
+              style={{
+                color: '#fff',
+                fontWeight: 'bold',
+                textAlign: 'center'
+              }}
+            >
+              Take picture
+            </Text>
+          </TouchableOpacity>
+          <View
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              flexDirection: 'row',
+              flex: 1,
+              width: '100%',
+              padding: 20,
+              justifyContent: 'space-between'
+            }}
+          >
+            <View
+              style={{
+                alignSelf: 'center',
+                flex: 1,
+                alignItems: 'center'
+              }}
+            >
+              <TouchableOpacity
+                onPress={__takePicture}
+                style={{
+                  width: 70,
+                  height: 70,
+                  bottom: 0,
+                  borderRadius: 50,
+                  backgroundColor: '#fff'
+                }}
+              />
+            </View>
+          </View>
+        </View>
 
+      )}
       <StatusBar style="auto" />
     </View>
   )
@@ -62,3 +112,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   }
 })
+registerRootComponent(App);
